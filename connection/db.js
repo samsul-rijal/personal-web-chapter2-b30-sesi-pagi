16966c61d343dc31bae20c31021f2513cb2c7c7f
Samsul Rijal <72549004+samsul-rijal@users.noreply.github.com>
2022-01-22 01:15:31 +0700

    Update db.js

--- connection/db.js
+++ connection/db.js
@@ -2,10 +2,11 @@
 const {Pool} = require('pg')
 
 const dbPool = new Pool({
-    database: 'personal_web_b30',
+    host: 'ec2-18-234-17-166.compute-1.amazonaws.com
+    database: 'd2mu8rsbl5p1ru',
     port: 5432,
-    user: 'postgres',
-    password: 'ADMIN'
+    user: 'qkqcyqpyrfeerj',
+    password: '54626b6c73c31bbb59f2dff1530633c6e27510c289a84378b4d7e371bd7d7ff1'
 })
 
 
